ac37bdf6aef46cd421a71039b336b1f19b647460
MammadDevelop <Mammad.Develop@outlook.com>
2024-08-17 16:53:57 +0330

    add ViewModel

---
+++ ViewModel/ChangePass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    public class ChangePass
+    {
+        [Display(Name = "رمز عبور سابق ")]
+        [Required(ErrorMessage = "{0}  نمیتواند خالی باشد")]
+        [DataType(DataType.Password)]
+        public string OldPassword { get; set; }
+        [Display(Name = "رمز عبور")]
+        [Required(ErrorMessage = "{0}  نمیتواند خالی باشد")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "تکرار رمز عبور")]
+        [Required(ErrorMessage = "{0}  نمیتواند خالی باشد")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "تکرار رمز عبور  مطابقت ندارد")]
+        public string RepPassword { get; set; }
+    }
+}
---
+++ ViewModel/Login.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    public class Login
+    {
+        [Display(Name = "نام کاربری")]
+        [Required(ErrorMessage = "{0}  نمیتواند خالی باشد")]
+        public string UserName { get; set; }
+
+        [Display(Name = "رمز عبور")]
+        [Required(ErrorMessage = "{0}  نمیتواند خالی باشد")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
